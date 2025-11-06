282d3be5dd380c4adcdb15183359ec9ba58e8b04
Federico Luna Agüero <aguerofedea@gmail.com>
2025-11-06 14:44:08 -0300

    chore: add iac alias file

---
+++ zsh/.zsh/iac.zsh
@@ -0,0 +1,14 @@
+# Terragrunt aliases
+alias tg='terragrunt'
+alias tgi='terragrunt init'
+alias tgp='terragrunt plan'
+alias tga='terragrunt apply'
+alias tgd='terragrunt destroy'
+alias tgo='terragrunt output'
+alias tgfu="terragrunt force-unlock"
+alias tgfmt='terragrunt fmt'
+alias tgshow='terragrunt show'
+alias tggraph='terragrunt graph'
+alias tgvalidate='terragrunt validate'
+
+alias tgformat='terragrunt format'
